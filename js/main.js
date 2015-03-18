d8c5f0a8ef5c1904e88d660cacc9443bbe2e5087
Sharmine <slxmlow@gmail.com>
2015-03-18 22:43:46 +0800

    update URL for Student Login

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$(function() {
+	$( "#studentLogin" ).click(function() {
+		window.location.href = "https://ivle.nus.edu.sg/api/login/?apikey=3bBGOIdtC1T2d7SXeQAO9&url=http://steps.tk/php/login.php";
+	});
+});
