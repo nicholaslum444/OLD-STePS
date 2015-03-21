8580b8bb2f59daac6ce9c772e8921046ccd3462c
Sharmine <slxmlow@gmail.com>
2015-03-22 05:41:09 +0800

    Complete first version of forms

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 $(function() {
 	$( "#studentLogin" ).click(function() {
-		/* To be changed when landing page is finalised */
 		// a popup box would be nicer :)
-		window.location.href = "https://ivle.nus.edu.sg/api/login/?apikey=3bBGOIdtC1T2d7SXeQAO9&url=http://steps.tk/student/";
+		/* Still trying to get the child window redirect the parent page */
+		openInNewWindow("https://ivle.nus.edu.sg/api/login/?apikey=3bBGOIdtC1T2d7SXeQAO9&url=http://steps.tk/student/");	
 	});
 });
 
 $(function() {
     $( "#lecturerLogin" ).click(function() {
-        /* To be changed when landing page is finalised */
         // a popup box would be nicer :)
         window.location.href = "https://ivle.nus.edu.sg/api/login/?apikey=3bBGOIdtC1T2d7SXeQAO9&url=http://steps.tk/lecturer/";
     });
 });
+
+function openInNewWindow(url) {
+	var newWindow = window.open(url,'name','height=500,width=700');
+	if (window.focus) {
+		newWindow.focus();
+		return newWindow;
+	}
+	return false;
+}
